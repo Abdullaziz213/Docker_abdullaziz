734f740e7207cbf8bd8ac0be768f2d9f4a7a85c9
Abdullaziz Hassen Abdella <abdullaziz.hassenabdella@ksb-sg.ch>
2024-05-05 15:00:07 +0200

    Docker

---
+++ aufgabe341.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aflac</title>
+</head>
+<body>
+    <img src="logo.png" alt="logo">
+    <hr>
+    <p>Max Mustermann</p>
+    <p>Hauptstrasse 65</p>
+    <p>5000 Aarau</p>
+    <hr>
+    <p>Hans Muster</p>
+    <p>Musterstrasse 5</p>
+    <p>9000 St.Gallen</p>
+    <hr>
+    <p>Bern, 20.April 2018</p><br><br><br>
+    <hr>
+    <h1>Rechnung 18-4465</h1><br><br>
+    <hr>
+    <p>Gerne stellen wir ihnen folgende Arbeiten in Rechnung.</p><br><br>
+    <hr>
+    <table>
+        <tr>
+            <th>
+                Pos.
+            </th>
+            <th>
+                Beschreibung
+            </th>
+            <th>
+                Stückpreis CHF
+            </th>
+            <th>
+                Menge
+            </th>
+            <th>
+                Preis CHF
+            </th>
+        </tr>
+        <tr>
+            <td>
+               1 
+            </td>
+            <td>
+                Computer
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ dockerfile
@@ -0,0 +1,2 @@
+FROM php:8-apache
+COPY aufgabe341.php /var/www/html
